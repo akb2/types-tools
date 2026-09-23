8c43da68afa521e9dfa932220eed833af81b30b2
akb2 <akb2-online@ya.ru>
2026-09-23 13:25:46 +0400

    Added missed exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { anyToArray, anyToBoolean, anyToDate, anyToFloat, anyToInt, anyToString } from './converters';
 export { createArray, isDefined } from './methods';
-export type { CustomObject, CustomObjectKey, Delta, MultiArray, MultiObject, NotDefinable, Nullable, SimpleObject, Undefinable } from './models';
+export type * from './models';
 export { escapeRegExp, smartJoin } from './strings';
